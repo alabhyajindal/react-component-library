7d539ba505449dd9defa1ab13ebdab05466d2097
Alabhya Jindal <alabhya10@gmail.com>
2023-04-22 23:44:40 +0530

    Rollup working

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import { createRequire } from 'node:module';
 
+const require = createRequire(import.meta.url);
 const packageJson = require('./package.json');
 
 export default [
