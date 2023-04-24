0841ac65461ca76f625808c8e76d2735351ea5f9
Alabhya Jindal <alabhya10@gmail.com>
2023-04-24 11:26:31 +0530

    Storybook working

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const HelloWorld: Story = {
+  args: {
+    label: 'Hello World',
+  },
+};
+
+export const SupBruh: Story = {
+  args: {
+    label: 'Sup Bruh',
+  },
+};
